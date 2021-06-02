f18933f69e25b7c9c25965a2f268e3e48a214f3c
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-02 21:04:58 +0530

    Deleted some code

--- lib/Widget/item_widgets.dart
+++ lib/Widget/item_widgets.dart
@@ -9,29 +9,6 @@ class CatlogWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    // return Card(
-    //   clipBehavior: Clip.antiAlias,
-    //   elevation:12,
-    //   shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)) ,
-    //   child: GridTile(
-    //     header: Container(
-    //       padding: EdgeInsets.all(12),
-    //         decoration: BoxDecoration(
-    //           color: Colors.deepPurple
-    //         ),
-    //         child: Text(iteam.name,style: TextStyle(color: Colors.white),)
-    //     ),
-    //     child: Image.network(iteam.image,fit: BoxFit.cover,),
-    //     footer:  Container(
-    //         padding: EdgeInsets.all(12),
-    //         decoration: BoxDecoration(
-    //             color: Colors.black
-    //         ),
-    //         child: Text("\$${iteam.price}",style: TextStyle(color: Colors.white),)
-    //     ),
-    //   ),
-    //   );
-    //);
     return Card(
       shape: StadiumBorder() ,
       child: ListTile(
