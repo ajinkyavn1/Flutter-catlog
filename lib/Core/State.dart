37d7e9751f184dc407aef02a8b4f6f17de5b0307
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-02 18:53:27 +0530

    in this commit we developed some state management classs

--- lib/Core/State.dart
+++ lib/Core/State.dart
@@ -1,5 +1,13 @@
+import 'package:frist_flutter/Models/Catlog.dart';
+import 'package:frist_flutter/Models/cartModel.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class MyStore extends VxStore{
-  
+  CatlogModel catlog;
+  CartModel cart;
+  MyStore(){
+    cart =CartModel();
+    catlog=CatlogModel();
+    cart.catalog=catlog;
+  }
 }
